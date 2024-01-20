1f6ced242ba9af718d76a70788df2298b437c261
Samira Hafezi <samirahafezi@Samiras-MacBook-Pro-3.local>
2024-01-19 23:02:07 -0500

    adding debug toolbar

--- cityevents/urls.py
+++ cityevents/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,4 +23,5 @@ from torontoevents import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('torontoevents.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
